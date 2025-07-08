178227e9bcfff6460dbdcb6e8eada5b72f17e821
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-07-08 10:21:49 +0545

    Update server configuration: added bcryptjs, express-validator, and jsonwebtoken dependencies; refactored index.js for MongoDB connection; modified .gitignore; updated package-lock.json for dependency changes.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 // Load environment variables
 dotenv.config();
@@ -13,14 +13,21 @@ app.use(cors());
 app.use(express.json());
 
 // Basic route
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Movie Booking API' });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Movie Booking API" });
 });
 
-// Port configuration
-const PORT = process.env.PORT || 5000;
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Port configuration
+    const PORT = process.env.PORT || 5000;
+    // Start server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port  http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
