afe9c1efac2fe07f67c15927ac1612e9823adf42
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-07-11 13:27:27 +0545

    Enhance Footer and Movies components: Updated Footer component to include branding, company information, and contact details. Improved Movies component to display a list of movies using dummy data, with a fallback message for no available movies. Added visual elements for better user experience.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="px-6 md:px-16 mt-40  lg:px-36 xl:px-32 pt-8 w-full text-gray-300">
+      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-14">
+        <div className="md:max-w-96">
+          <img src={assets.logo} className="w-36 h-auto" alt="Logo" />
+          <p className="mt-6 text-sm">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <div className="flex items-center gap-2 mt-4">
+            <img
+              src={assets.googlePlay}
+              className="w-auto h-10"
+              alt="Playstore"
+            />
+            <img src={assets.appStore} className="w-auto h-10" alt="Appstore" />
+          </div>
+        </div>
+        <div className="flex-1 flex items-start md:justify-end gap-20">
+          <div>
+            <h2 className="font-semibold mb-5 text-primary">Company</h2>
+            <ul className="text-sm space-y-2">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About us</a>
+              </li>
+              <li>
+                <a href="#">Contact us</a>
+              </li>
+              <li>
+                <a href="#">Privacy policy</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-semibold mb-5 text-primary">Get in touch</h2>
+            <div className="text-sm space-y-2">
+              <p>+1-212-456-7890</p>
+              <p>contact@example.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="pt-4 text-center text-xs md:text-sm pb-5">
+        Copyright 2024 © Movie Booking. All Right Reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
