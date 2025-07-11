95212062b20c0dca4c12be9cb7a12476dac06b11
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-07-11 11:31:49 +0545

    Refactor assets and enhance FeaturedSection: Updated assets.js to improve data structure and added dummy data for shows. Modified FeaturedSection to display movie cards using the new data structure, enhancing the user interface. Updated MovieCard component to include additional movie details and improved styling.

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -2,7 +2,8 @@ import { ArrowRight } from "lucide-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import BlurCircle from "./BlurCircle";
-import { assets } from "../assets/assets";
+import { dummyShowsData } from "../assets/assets";
+import MovieCard from "./MovieCard";
 
 const FeaturedSection = () => {
   const navigate = useNavigate();
@@ -20,7 +21,11 @@ const FeaturedSection = () => {
         </button>
       </div>
 
-      <div className=""></div>
+      <div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
+        {dummyShowsData.slice(0, 4).map((show) => (
+          <MovieCard key={show._id} movie={show} />
+        ))}
+      </div>
 
       <div className="flex justify-center mt-20 mb-10">
         <button
--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { StarIcon } from "lucide-react";
+import timeformat from "../lib/timeformat";
 
 const MovieCard = ({ movie }) => {
   const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col justify-between p-3 bg-gray-800 rounded-3xl hover:-translate-y-1 transition-duration-300 w-66 ">
+    <div className="flex flex-col justify-between p-3 bg-gray-800 rounded-3xl hover:-translate-y-1 transition-duration-00 w-66 ">
       <img
         onClick={() => {
           navigate(`/movies/${movie._id}`);
@@ -16,14 +18,30 @@ const MovieCard = ({ movie }) => {
         className="rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer"
       />
       <p className="font-semibold mt-2 truncate">{movie.title}</p>
-      <p className="text-sm mt-2 tex-gray">
-        {new Date(movie.release_date).getFullYear} ·{" "}
+      <p className="text-sm mt-2 text-gray-400">
+        {new Date(movie.release_date).getFullYear()} ·{" "}
         {movie.genres
           .slice(0, 2)
           .map((genre) => genre.name)
           .join(" | ")}{" "}
-        · {movie.runtime}
+        · {timeformat(movie.runtime)}
       </p>
+
+      <div className="flex justify-between items-center mt-4 pb-3 ">
+        <button
+          onClick={() => {
+            navigate(`/movies/${movie._id}`);
+            scrollTo(0, 0);
+          }}
+          className="bg-primary rounded-full px-4 py-2 cursor-pointer hover:bg-primary-dull tex-xs font-medium transition-all duration-300"
+        >
+          Buy Tickets
+        </button>
+        <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+          <StarIcon className="w-4 h-4 text-primary fill-primary" />
+          {movie.vote_average.toFixed(1)}
+        </p>
+      </div>
     </div>
   );
 };
