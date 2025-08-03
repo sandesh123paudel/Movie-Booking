8350bfdabe53027a6943dd128d1bbb77a49801dd
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-08-03 22:54:10 +0545

    Updated..

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { AppContextProvider } from "./context/AppContext.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Import } from "lucide-react";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
